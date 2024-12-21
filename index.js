cac77456654bc0e462df03b82525ae5042de92f0
Zaynab El Hakour <elhakourzaynab@gmail.com>
2024-12-21 01:04:37 +0000

    FCC-File-metadata-microservice-Solution

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 
 var app = express();
 
@@ -10,8 +13,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size 
+    });
+    } catch (err) {
+      res.send(400);
+    }
+  });
 
-
+  
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
 
 
 const port = process.env.PORT || 3000;
